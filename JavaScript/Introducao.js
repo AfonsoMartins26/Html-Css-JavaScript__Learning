e5d5c0a2c7ee80594a79a9f486137c508a6bb557
Afonso Martins <145707571+AfonsoMartins26@users.noreply.github.com>
2023-12-24 00:00:46 +0000

    Create Introducao.js

---
+++ JavaScript/Introducao.js
@@ -0,0 +1,20 @@
+// isto é um comentário em linha em javascript
+/*
+isto é um comentário de varisa linhas em javascript
+*/
+/*console.log("I like pizza");  // a mensagem "I like pizza" vai aparecer na consola
+console.log("its really good");*/
+ // window.alert("PIZZAAAAAAAAAAAAAAAAAAAA")  // isto vai criar um alerta na pagina a dizer pizzaaa
+// para abrir e executar um ficheiro tenho de abrir com o live server inspecionar a pagina 
+// e assim abrir a colsola e ver os resultados na pagina Html
+let  age;
+age=18;
+
+let fistname = "Afonso";
+
+console.log(age);
+console.log("Hello",fistname)
+
+document.getElementById("p1").innerHTML = "Hello " + fistname;
+// document.write("Hello "+fistname);  escreve diretamente no html
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
